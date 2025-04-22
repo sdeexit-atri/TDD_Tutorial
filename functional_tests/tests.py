5505e63aa486f827451dd35816d6d5528b2094ff
sdeexit-atri <deexit@atrihub.io>
2025-04-22 14:55:50 -0700

    feat(Chapter6): TDD Training
    Improving Functional Tests: Ensuring Isolation and Removing Voodoo Sleep
    
    [ICTRAINING-2043]

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,45 +1,48 @@
+import time
+
+from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
-import unittest
+
+MAX_WAIT = 5
 
 
-class NewVisitorTest(unittest.TestCase):  
-    def setUp(self):  
-        self.browser = webdriver.Firefox()  
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-    def tearDown(self):  
+    def tearDown(self):
         self.browser.quit()
-    
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_todo_list(self):  
+    def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, "h1").text  
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
         self.assertIn("To-Do", header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element(By.ID, "id_new_item")  
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
         self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # She types "Buy peacock feathers" into a text box
         # (Edith's hobby is tying fly-fishing lures)
-        inputbox.send_keys("Buy peacock feathers")  
+        inputbox.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
@@ -47,11 +50,24 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-    # The page updates again, and now shows both items on her list
+        # The page updates again, and now shows both items on her list
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
+        # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
-if __name__ == "__main__":  
-    unittest.main() 
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
