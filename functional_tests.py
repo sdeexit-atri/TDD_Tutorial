e8bcca4ba91a3aa20d04ce964fb36952cc762c0c
sdeexit-atri <deexit@atrihub.io>
2025-04-22 10:39:49 -0700

    feat(Chapter5): TDD Training
    Saving User Input: Testing the Database
    
    [ICTRAINING-2042]

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):  
         self.browser.quit()
+    
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_todo_list(self):  
         # Edith has heard about a cool new online to-do app.
@@ -32,26 +37,21 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(Keys.ENTER)  
-        time.sleep(1)  
-
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")  
-        self.assertTrue(
-        any(row.text == "1: Buy peacock feathers" for row in rows),
-        "New to-do item did not appear in table",
-    )
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
-        self.fail("Finish the test!")
-  
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        [...]
-
-        # Satisfied, she goes back to sleep
-
+    # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
 if __name__ == "__main__":  
     unittest.main() 
