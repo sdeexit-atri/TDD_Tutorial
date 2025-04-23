4a86c4afc3400f046e52a8b3d47e712d39f0ea91
sdeexit-atri <deexit@atrihub.io>
2025-04-23 14:05:04 -0700

    refactor(Chapter7): TDD Training
    Working Incrementally
    
    [ICTRAINING-2044]

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,24 @@
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item
+
+from lists.models import Item, List
 
 
 def home_page(request):
-    if request.method == "POST":
-        Item.objects.create(text=request.POST["item_text"])
-        return redirect("/")
+    return render(request, "home.html")
 
-    items = Item.objects.all()
-    return render(request, "home.html", {"items": items})
+
+def new_list(request):
+    nulist = List.objects.create()
+    Item.objects.create(text=request.POST["item_text"], list=nulist)
+    return redirect(f"/lists/{nulist.id}/")
+
+
+def add_item(request, list_id):
+    our_list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=our_list)
+    return redirect(f"/lists/{our_list.id}/")
+
+
+def view_list(request, list_id):
+    our_list = List.objects.get(id=list_id)
+    return render(request, "list.html", {"list": our_list})
